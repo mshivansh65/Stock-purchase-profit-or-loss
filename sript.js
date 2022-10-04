e05ab81ea40d9963ada55dc7a82311cd18f6517e
mshivansh65 <77801649+mshivansh65@users.noreply.github.com>
2022-10-04 13:35:59 +0530

    fix
    handled empty negative inputs

--- sript.js
+++ sript.js
@@ -3,14 +3,42 @@ const initalValueEl = document.querySelector(".initalValue");
 const initialQuantityEl = document.querySelector(".initialQuantity");
 const finalValueEl = document.querySelector(".finalValue");
 const messageEl = document.querySelector(".message");
+// overlay
+const overlay = document.querySelector(".overlay");
+const message = document.querySelector("#message");
+const backdrop = document.querySelector(".backdrop");
+const overlayBtn = document.querySelector("#overlay-btn");
+
+function showMessageOverlay(recivedMessage = "") {
+  overlay.classList.remove("hidden");
+  backdrop.classList.remove("hidden");
+  message.innerHTML = recivedMessage;
+}
+function hideOverlay() {
+  overlay.classList.add("hidden");
+  backdrop.classList.add("hidden");
+}
+hideOverlay();
+overlayBtn.addEventListener("click", hideOverlay);
+backdrop.addEventListener("click", hideOverlay);
+// END OVERLAY CODE
 form.addEventListener("submit", submitHandler);
 function submitHandler(e) {
   e.preventDefault();
   const initalValue = Number(initalValueEl.value);
   const initialQuantity = Number(initialQuantityEl.value);
   const finalValue = Number(finalValueEl.value);
-  if (initalValue && initialQuantity && finalValue)
+  if (initalValue === 0 || initialQuantity === 0) {
+    showMessageOverlay(`Initial Quantity Can't be zero`);
+  } else if (initalValue < 0 || initialQuantity < 0 || finalValue < 0) {
+    showMessageOverlay("Number's cant be negative");
+  } else if (initalValue && initialQuantity && finalValue === 0) {
     calculateProfitAndLoss(initalValue, initialQuantity, finalValue);
+  } else if (initalValue && initialQuantity && finalValue) {
+    calculateProfitAndLoss(initalValue, initialQuantity, finalValue);
+  } else {
+    showMessageOverlay("Please Enter Something");
+  }
 }
 function calculateProfitAndLoss(initalValue, initialQuantity, finalValue) {
   if (initalValue < finalValue) {
